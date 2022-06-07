fc6a1e026d701867d4071afd157440eccf4db6af
DSAghicha <dsaghicha211@gmail.com>
2022-06-07 22:21:25 +0530

    Created Task View Component

---
+++ client/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-task-view',
+    templateUrl: './task-view.component.html',
+    styleUrls: ['./task-view.component.sass']
+})
+export class TaskViewComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
